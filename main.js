341792fc77ca9b6d9fe2e2aead96f6150ef5b209
Nicolas Jourdain <nicolas.jourdain@randstad.ca>
2017-11-04 15:01:52 -0400

    Make electron open dev tools in dev

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ function createWindow () {
 
   // and load the index.html of the app.
   mainWindow.loadURL('http://localhost:4200')
+  mainWindow.webContents.openDevTools();
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
